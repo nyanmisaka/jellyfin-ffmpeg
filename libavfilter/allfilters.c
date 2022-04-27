852462bc9a0820d7dc76dedccecb54d653db5298
nyanmisaka <nst799610810@gmail.com>
2022-04-27 22:33:07 +0800

    opencl-scale

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -400,6 +400,7 @@ extern const AVFilter ff_vf_sab;
 extern const AVFilter ff_vf_scale;
 extern const AVFilter ff_vf_scale_cuda;
 extern const AVFilter ff_vf_scale_npp;
+extern const AVFilter ff_vf_scale_opencl;
 extern const AVFilter ff_vf_scale_qsv;
 extern const AVFilter ff_vf_scale_vaapi;
 extern const AVFilter ff_vf_scale_vulkan;
