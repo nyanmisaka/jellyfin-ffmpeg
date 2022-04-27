9127fdd29ba5192c223fa71c63dcf4570459562c
nyanmisaka <nst799610810@gmail.com>
2022-04-28 01:00:28 +0800

    fix webvttenc

--- libavformat/webvttenc.c
+++ libavformat/webvttenc.c
@@ -49,8 +49,8 @@ static int webvtt_write_header(AVFormatContext *ctx)
     AVCodecParameters *par = ctx->streams[0]->codecpar;
     AVIOContext *pb = ctx->pb;
 
-    if (ctx->nb_streams != 1 || par->codec_id != AV_CODEC_ID_WEBVTT) {
-        av_log(ctx, AV_LOG_ERROR, "Exactly one WebVTT stream is needed.\n");
+    if (par->codec_id != AV_CODEC_ID_WEBVTT) {
+        av_log(ctx, AV_LOG_ERROR, "First stream must be WebVTT.\n");
         return AVERROR(EINVAL);
     }
 
@@ -67,6 +67,9 @@ static int webvtt_write_packet(AVFormatContext *ctx, AVPacket *pkt)
     size_t id_size, settings_size;
     int id_size_int, settings_size_int;
     uint8_t *id, *settings;
+
+    if (pkt->stream_index != 0)
+        return 0;
 
     avio_printf(pb, "\n");
 
