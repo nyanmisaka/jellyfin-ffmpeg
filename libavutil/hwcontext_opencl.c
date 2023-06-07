bad3def5e85778946e5a384fb44ae0bb73596c78
nyanmisaka <nst799610810@gmail.com>
2023-06-08 01:31:29 +0800

    drm-cl-arm
    Signed-off-by: nyanmisaka <nst799610810@gmail.com>

--- libavutil/hwcontext_opencl.c
+++ libavutil/hwcontext_opencl.c
@@ -83,6 +83,16 @@ typedef CL_API_ENTRY cl_mem(CL_API_CALL *clConvertImageAMD_fn)(
 #include <CL/cl_ext.h>
 #include <drm_fourcc.h>
 #include "hwcontext_drm.h"
+
+typedef intptr_t cl_import_properties_arm;
+typedef CL_API_ENTRY cl_mem(CL_API_CALL *clImportMemoryARM_fn)(
+    cl_context context,
+    cl_mem_flags flags,
+    const cl_import_properties_arm *properties,
+    void *memory,
+    size_t size,
+    cl_int *errcode_ret);
+
 #endif
 
 #if HAVE_OPENCL_VAAPI_INTEL_MEDIA && CONFIG_LIBMFX
@@ -150,6 +160,9 @@ typedef struct OpenCLDeviceContext {
 
 #if HAVE_OPENCL_DRM_ARM
     int drm_arm_mapping_usable;
+
+    clImportMemoryARM_fn
+        clImportMemoryARM;
 #endif
 } OpenCLDeviceContext;
 
@@ -923,7 +936,8 @@ static int opencl_device_init(AVHWDeviceContext *hwdev)
             fail = 1;
         }
 
-        // clImportMemoryARM() is linked statically.
+        CL_FUNC(clImportMemoryARM,
+                "DRM to OpenCL mapping on ARM");
 
         if (fail) {
             av_log(hwdev, AV_LOG_WARNING, "DRM to OpenCL mapping on ARM "
@@ -3058,6 +3072,7 @@ static int opencl_map_from_drm_arm(AVHWFramesContext *dst_fc, AVFrame *dst,
     AVHWFramesContext *src_fc =
         (AVHWFramesContext*)src->hw_frames_ctx->data;
     AVOpenCLDeviceContext *dst_dev = dst_fc->device_ctx->hwctx;
+    OpenCLDeviceContext *device_priv = dst_fc->device_ctx->internal->priv;
     const AVDRMFrameDescriptor *desc;
     DRMARMtoOpenCLMapping *mapping = NULL;
     cl_mem_flags cl_flags;
@@ -3091,8 +3106,8 @@ static int opencl_map_from_drm_arm(AVHWFramesContext *dst_fc, AVFrame *dst,
         }
 
         mapping->object_buffers[i] =
-            clImportMemoryARM(dst_dev->context, cl_flags, props,
-                              &fd, desc->objects[i].size, &cle);
+            device_priv->clImportMemoryARM(dst_dev->context, cl_flags, props,
+                                           &fd, desc->objects[i].size, &cle);
         if (!mapping->object_buffers[i]) {
             av_log(dst_fc, AV_LOG_ERROR, "Failed to create CL buffer "
                    "from object %d (fd %d, size %"SIZE_SPECIFIER") of DRM frame: %d.\n",
