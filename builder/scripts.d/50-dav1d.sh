4a8b70f9226852cf1024d64c95367f241cba25fa
gnattu <gnattuoc@me.com>
2025-10-07 05:30:13 +0800

    bump dav1d to include nasm v3 fix

--- builder/scripts.d/50-dav1d.sh
+++ builder/scripts.d/50-dav1d.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://code.videolan.org/videolan/dav1d.git"
-SCRIPT_COMMIT="af5cf2b1e7f03d6f6de84477e1ca8eed1f3eb03d"
+SCRIPT_COMMIT="0bc6bd93417179cd0c30fac40d2fd11aa29c8523"
 
 ffbuild_enabled() {
     return 0
